3bba20b30038500ef7f13e8f875ff0d8381c3356
Abdullah Ibne Atiq <hello@abdu.io>
2020-08-25 16:10:03 +0100

    Create Section component

---
+++ src/components/Section.svelte
@@ -0,0 +1,12 @@
+<section class={$$props.class}>
+    <slot />
+</section>
+
+<style>
+    section {
+        width: 80%;
+        max-width: 80rem;
+        padding: 3rem 1.5rem;
+        margin: 0 auto;
+    }
+</style>
