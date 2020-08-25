a9e416fa45abb3e56518580929e3227946c5f349
Abdullah Ibne Atiq <hello@abdu.io>
2020-08-25 15:58:34 +0100

    Create Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer>
+    <p>&copy; 2016 - {new Date().getFullYear()} Abdullah Ibne Atiq</p>
+</footer>
+
+<style>
+    footer {
+        font-size: 0.9rem;
+        text-align: center;
+        width: 80%;
+        max-width: 80rem;
+        padding: 3rem 1.5rem;
+        margin: 0 auto;
+    }
+
+    @media only screen and (min-width: 990px) {
+        footer {
+            text-align: initial;
+        }
+    }
+</style>
