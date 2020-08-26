2584217df556ff53a43facc6a59ea107081e2976
Abdullah Ibne Atiq <hello@abdu.io>
2020-08-26 12:26:05 +0100

    Create intro section component

---
+++ src/components/sections/Intro.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Section from "../Section.svelte"
+</script>
+
+<Section class="intro">
+    <slot/>
+</Section>
+
+<style lang="scss">
+    :global(.intro) {
+        display: flex;
+
+        & > :global(div) {
+            flex: 1;
+            padding: 2rem;
+        }
+
+        :global(h1) {
+            font-size: 2.5rem;
+        }
+
+        :global(p) {
+            font-size: 1.5rem;
+        }
+    }
+</style>
