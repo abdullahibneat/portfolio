638dcd1bac02891b46aa2cdc0299ca8c756e990c
Abdullah Ibne Atiq <hello@abdu.io>
2020-08-26 12:28:44 +0100

    Change title, use intro section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,17 @@
 <script>
-	import Section from "../components/Section.svelte";
-
-	export let name = "World"
+	import Intro from "../components/sections/Intro.svelte";
 </script>
 
 <svelte:head>
-	<title>Sapper</title>
+	<title>Home | Abdullah Ibne Atiq</title>
 </svelte:head>
 
-<Section>
-	<h1>Hello, {name}</h1>
-</Section>
+<Intro>
+	<div>
+		<h1>Lorem ipsum dolor sit amet</h1>
+		<p>Consectetur adipiscing elit. Mauris in est scelerisque, scelerisque risus vel, tincidunt dolor.</p>
+	</div>
+	<div style="display: flex; align-items: center; justify-content: center;">
+		<p>Latest project here</p>
+	</div>
+</Intro>
