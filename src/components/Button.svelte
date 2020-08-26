37d329e5f10ee10b357520bfd6bbc2bfe0230f5c
Abdullah Ibne Atiq <hello@abdu.io>
2020-08-26 12:50:46 +0100

    Create Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let label = ""
+    export let onClick = () => {}
+</script>
+
+<button on:click={onClick}>{label}</button>
+
+<style lang="scss">
+    button {
+        outline: none;
+        padding: .5rem 1rem;
+        border: none;
+        border-radius: .75rem;
+        background: black;
+        color: white;
+        box-shadow: var(--box-shadow);
+        transition: transform 250ms ease;
+        font-weight: bold;
+
+        &:hover {
+            transform: scale(1.05);
+        }
+    }
+</style>
