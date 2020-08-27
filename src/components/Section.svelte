b01f1618035159003c88fddbf86eb3b1b9b1cd88
Abdullah Ibne Atiq <hello@abdu.io>
2020-08-27 19:16:26 +0100

    Increase width on mobile

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -2,11 +2,15 @@
     <slot />
 </section>
 
-<style>
+<style lang="scss">
     section {
-        width: 80%;
+        width: 90%;
         max-width: 80rem;
         padding: 3rem 1.5rem;
         margin: 0 auto;
+
+        @media only screen and (min-width: 990px) {
+            width: 80%;
+        }
     }
 </style>
