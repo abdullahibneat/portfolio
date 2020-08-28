eece1937aa17036de5590cbdcc5d694d7e803ce2
Abdullah Ibne Atiq <hello@abdu.io>
2020-08-28 13:12:29 +0100

    Create grid for projects

---
+++ src/components/ProjectsGrid.svelte
@@ -0,0 +1,28 @@
+<script>
+import ProjectCard from "./ProjectCard.svelte";
+
+    export let projects = []
+</script>
+
+<div class="container">
+    {#each projects as project }
+        <ProjectCard class="project" {...project} />
+    {/each}
+</div>
+
+<style lang="scss">
+    .container {
+        display: flex;
+        flex-wrap: wrap;
+
+        :global(.project) {
+            @media only screen and (min-width: 600px) {
+                width: 50%;
+            }
+
+            @media only screen and (min-width: 990px) {
+                width: 33.3333%;
+            }
+        }
+    }
+</style>
