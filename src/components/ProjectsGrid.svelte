e2ee18a10ed01aee101234a315f4a240a973df6c
Abdullah Ibne Atiq <hello@abdu.io>
2020-09-02 11:02:11 +0100

    Allow grid to be scrollable horizontally

--- src/components/ProjectsGrid.svelte
+++ src/components/ProjectsGrid.svelte
@@ -2,9 +2,10 @@
 import ProjectCard from "./ProjectCard.svelte";
 
     export let projects = []
+    export let horizontalScroll = false
 </script>
 
-<div class="container">
+<div class="container" class:horizontalScroll>
     {#each projects as project }
         <ProjectCard class="project" {...project} />
     {/each}
@@ -22,6 +23,36 @@ import ProjectCard from "./ProjectCard.svelte";
 
             @media only screen and (min-width: 990px) {
                 width: 33.3333%;
+            }
+        }
+    }
+
+    .horizontalScroll {
+        width: 100%;
+        flex-wrap: nowrap;
+        overflow-x: auto;
+        padding-bottom: 2rem;
+
+        :global(.project) {
+            flex: 0 0 100%;
+            position: relative;
+
+            &:first-of-type {
+                margin-left: 7rem;
+            }
+
+            // margin-right does not work for last element, use absolute positioning
+            &:last-of-type:after {
+                content: "";
+                display: block;
+                position: absolute;
+                height: 20px;
+                width: 7rem;
+                right: -7rem;
+            }
+
+            @media only screen and (min-width: 600px) {
+                flex: 0 0 50%;
             }
         }
     }
