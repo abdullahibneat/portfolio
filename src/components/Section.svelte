268c8f962cd842c99a923a84c451ce9136cd0f1d
Abdullah Ibne Atiq <hello@abdu.io>
2020-09-02 10:32:01 +0100

    Add optional fullWidth class

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -1,16 +1,26 @@
-<section class={$$props.class} style={$$props.style}>
+<script>
+    export let fullWidth = false
+</script>
+
+<section class:fullWidth class={$$props.class} style={$$props.style}>
     <slot />
 </section>
 
 <style lang="scss">
     section {
-        width: 90%;
-        max-width: 80rem;
-        padding: 3rem 1.5rem;
+        padding: 3rem 0;
         margin: 0 auto;
+        width: 100%;
+        max-width: 80rem;
 
-        @media only screen and (min-width: 990px) {
-            width: 80%;
+        &:not(.fullWidth) {
+            width: 90%;
+            padding-left: 1.5rem;
+            padding-right: 1.5rem;
+
+            @media only screen and (min-width: 990px) {
+                width: 80%;
+            }
         }
     }
 </style>
