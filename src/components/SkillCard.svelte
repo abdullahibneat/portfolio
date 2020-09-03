44014896e03c99adf6f1f9ba38d411c85618f656
Abdullah Ibne Atiq <hello@abdu.io>
2020-09-03 11:44:29 +0100

    Create skill card component and grid component

---
+++ src/components/SkillCard.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let name = ""
+    export let description = ""
+    export let urlLabel = ""
+    export let url = "#"
+</script>
+
+<div class={$$props.class} style={$$props.style}>
+    <h2>{name}</h2>
+    <p>{description}</p>
+    <a href={url}>{urlLabel}</a>
+</div>
+
+<style lang="scss">
+    div {
+        padding: 2rem 1rem 1rem;
+        border-radius: .75rem;
+        text-align: center;
+        background-image: var(--linear-gradient);
+        color: white;
+        box-shadow: var(--box-shadow);
+        transition: all 250ms ease;
+
+        h2 {
+            font-size: 1rem;
+        }
+
+        a {
+            text-decoration: none;
+            color: white;
+            font-size: .8rem;
+            float: right;
+        }
+
+        & > *:not(:last-child) {
+            margin-bottom: 1rem;
+        }
+
+        &:hover {
+            box-shadow: var(--box-shadow-hover-large);
+            transform: scale(1.01);
+        }
+    }
+</style>
---
+++ src/components/SkillCardGrid.svelte
@@ -0,0 +1,26 @@
+<script>
+    import SkillCard from "./SkillCard.svelte"
+    export let skills = []
+</script>
+
+<div class={$$props.class} style={$$props.style}>
+    {#each skills as skill}
+        <div class="skill">
+            <SkillCard {...skill} />
+        </div>
+    {/each}
+</div>
+
+<style lang="scss">
+    div {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        
+        .skill {
+            flex: 50%;
+            padding: .5rem;
+            max-width: 15rem;
+        }
+    }
+</style>
