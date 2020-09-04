d3820bed0929947391c4e9ce5c9cd60d0294f353
Abdullah Ibne Atiq <hello@abdu.io>
2020-09-04 18:52:15 +0100

    Show buttons to change testimonial, add scroll snap

--- src/components/sections/Testimonials.svelte
+++ src/components/sections/Testimonials.svelte
@@ -9,8 +9,13 @@
 
 <Section {title} {fullWidth}>
     <div class="testimonials">
-        {#each testimonials as testimonial}
-            <Testimonial class="testimonial" {...testimonial}/>
+        {#each testimonials as testimonial, i}
+            <Testimonial class="testimonial" id={`testimonial-${i}`} {...testimonial}/>
+        {/each}
+    </div>
+    <div class="circles">
+        {#each { length: testimonials.length } as _, i}
+            <a href={`#testimonial-${i}`} />
         {/each}
     </div>
 </Section>
@@ -22,11 +27,32 @@
         width: 100%;
         overflow-x: auto;
         overflow-y: hidden;
-        padding: 2rem 0;
         position: relative;
+        scroll-snap-type: x mandatory;
+        scroll-behavior: smooth;
 
         :global(.testimonial) {
+            padding-top: 1rem;
             flex: 0 0 100%;
+            scroll-snap-align: start;
+        }
+    }
+
+    .circles {
+        display: flex;
+        justify-content: center;
+        margin-top: 1rem;
+
+        a {
+            display: block;
+            width: .75rem;
+            height: .75rem;
+            border-radius: 50%;
+            background-color: #333;
+
+            &:not(:last-child) {
+                margin-right: .75rem;
+            }
         }
     }
 </style>
