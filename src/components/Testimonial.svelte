12c42ca428bc87e2ad8664e36ce507e34f05c3e0
Abdullah Ibne Atiq <hello@abdu.io>
2020-09-04 09:57:22 +0100

    Create testimonial component

---
+++ src/components/Testimonial.svelte
@@ -0,0 +1,47 @@
+<script>
+    export let text = ""
+    export let author = ""
+</script>
+
+<div class={$$props.class} style={$$props.style}>
+    <blockquote>{text}</blockquote>
+    <cite>{author}</cite>
+</div>
+
+<style lang="scss">
+    div {
+        text-align: center;
+    }
+
+    blockquote {
+        font-size: 2rem;
+        width: 80%;
+        margin: 0 auto;
+        position: relative;
+
+        // Quote marks
+        &:before, &:after {
+            position: absolute;
+            font-size: 7rem;
+            font-weight: bold;
+            color: var(--primary);
+        }
+
+        &:before {
+            content: "“";
+            top: -3rem;
+            left: -4rem;
+        }
+
+        &:after {
+            content: "”";
+            bottom: -5rem;
+            right: -5rem;
+        }
+    }
+
+    cite {
+        display: block;
+        margin-top: 1rem;
+    }
+</style>
