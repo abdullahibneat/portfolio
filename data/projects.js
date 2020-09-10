247f300e5865345e20de82dfb440e8d03873b197
Abdullah Ibne Atiq <hello@abdu.io>
2020-09-10 15:38:00 +0100

    Add skills to projects

--- data/projects.js
+++ data/projects.js
@@ -3,6 +3,7 @@ export default [
         "title": "Blog list",
         "summary": "A full stack application to save blogs, built to learn React and NodeJS. It features interacting with a MongoDB database, user authentication, testing and continuous integration through Vercel.",
         "featuredImage": "https://abdu.io/assets/media/BlogList/ft.jpg",
+        "skills": ["Full Stack", "Backend", "Databases", "JavaScript"],
         "projectURL": "https://abdu.io/project/blog-application/",
         "repoURL": "https://github.com/abdullahibneat/blog-backend"
     },
@@ -10,6 +11,7 @@ export default [
         "title": "Chat Room",
         "summary": "A Java peer-to-peer chatroom where people can send messages and communicate with each other",
         "featuredImage": "https://abdu.io/assets/media/ChatRoom/ft.jpg",
+        "skills": ["Java"],
         "projectURL": "https://abdu.io/project/chaat-room/",
         "repoURL": "https://github.com/abdullahibneat/p2p-chat-room"
     },
@@ -17,6 +19,7 @@ export default [
         "title": "PointAR",
         "summary": "My team and I identified a problem found in a number of sectors: integration. The solution? Augmented Reality, BYOD and Fujitsu Technology.",
         "featuredImage": "https://abdu.io/assets/media/PointAR/ft.jpg",
+        "skills": ["AR", "Unity", "C#"],
         "projectURL": "https://abdu.io/project/pointar/",
         "repoURL": "https://github.com/abdullahibneat/PointAR"
     },
@@ -24,6 +27,7 @@ export default [
         "title": "PointAR",
         "summary": "My team and I identified a problem found in a number of sectors: integration. The solution? Augmented Reality, BYOD and Fujitsu Technology.",
         "featuredImage": "https://abdu.io/assets/media/PointAR/ft.jpg",
+        "skills": ["AR", "Unity", "C#"],
         "projectURL": "https://abdu.io/project/pointar/",
         "repoURL": "https://github.com/abdullahibneat/PointAR"
     }
